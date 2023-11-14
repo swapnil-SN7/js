e9b06595f2787d0d272c2b42d4eaf3af1bc5a43c
swapnil-SN7 <127413524+swapnil-SN7@users.noreply.github.com>
2023-11-14 01:56:51 +0000

    datatypes are learned

---
+++ 01_basics/datatype.js
@@ -0,0 +1,20 @@
+"use strict"// treat all js code as newer version
+//alert("Hello") we are using node js not browser there is another way in node js to run the alert -> here it is giving the errror bcz i am running it in node js not in browser
+// two documentation are important tc39 and mdm for the javascript
+
+
+let name ="swapnil"
+let age =18
+let isLoggedin = false
+// number => 2 to power 53 
+// bigint 
+// string =>""
+//boolean=> false or true
+//null =>standalone value
+// undefined => abhi value is not defined
+//symbol => unique
+// object
+
+console.log(typeof "hitesh")
+console.log(typeof null)
+console.log(typeof )
