9b9895603e2b9fccd278a97d484449cebaedef3d
Shreyash7484 <shindeshreyah2900@gmail.com>
2023-11-15 07:29:21 +0530

    learnings never ends but life ends

---
+++ 01_basics/number.js
@@ -0,0 +1,20 @@
+// let score = 4000;
+// console.log(typeof score)
+
+
+//  const balance = new Number(100)
+//  console.log(typeof balance)
+
+//  const otherNumber = 100000
+//  console.log(otherNumber.toLocaleString('en-IN'))
+
+//++++++++++++++++++++++++++++++++++++++Maths
+
+console.log(Math);
+
+///random in js trick and point to be remember \
+const min =10;
+const max = 20;
+(Math.random()*(max-min)+1)+min
+
+//console pe ja and number pr konse property dekh le
---
+++ 01_basics/string.js
@@ -0,0 +1,4 @@
+const name = "swapnil"
+const repocount = 50
+//console.log(name + repocount+"value")->> aaj ke jamane me ye koi use nahi karta hai yar
+console.log(`Hello my name is ${name} and repo count is ${repocount}`)
